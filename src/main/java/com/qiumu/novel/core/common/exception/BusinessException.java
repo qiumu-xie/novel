4fa93ff6b8c835bd4ad8ae72365d8d1cdb840b86
qiumu <ling@qiumu.cn>
2023-08-07 16:03:13 +0800

    feat(core/common): 请求/响应数据格式封装和通用异常处理
    通用请求/响应数据格式封装，Rest接口响应工具及响应数据格式封装，通用异常处理

---
+++ src/main/java/com/qiumu/novel/core/common/exception/BusinessException.java
@@ -0,0 +1,27 @@
+package com.qiumu.novel.core.common.exception;
+
+import com.qiumu.novel.core.common.constant.ErrorCodeEnum;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 自定义业务异常，用于处理用户请求时，业务错误时抛出
+ *
+ * @author qiumu
+ * @date 2023/8/7
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCodeEnum errorCodeEnum;
+
+    public BusinessException(ErrorCodeEnum errorCodeEnum) {
+        // 不调用父类 Throwable的fillInStackTrace() 方法生成栈追踪信息，提高应用性能
+        // 构造器之间的调用必须在第一行
+        super(errorCodeEnum.getMessage(), null, false, false);
+        this.errorCodeEnum = errorCodeEnum;
+    }
+
+}
+
---
+++ src/main/java/com/qiumu/novel/core/common/exception/CommonExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.qiumu.novel.core.common.exception;
+
+import com.qiumu.novel.core.common.constant.ErrorCodeEnum;
+import com.qiumu.novel.core.common.resp.RestResp;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 通用的异常处理器
+ *
+ * @author qiumu
+ * @date 2023/8/7
+ */
+@Slf4j
+@RestControllerAdvice
+public class CommonExceptionHandler {
+
+    /**
+     * 处理数据校验异常
+     * */
+    @ExceptionHandler(BindException.class)
+    public RestResp<Void> handlerBindException(BindException e){
+        log.error(e.getMessage(),e);
+        return RestResp.fail(ErrorCodeEnum.USER_REQUEST_PARAM_ERROR);
+    }
+
+    /**
+     * 处理业务异常
+     * */
+    @ExceptionHandler(BusinessException.class)
+    public RestResp<Void> handlerBusinessException(BusinessException e){
+        log.error(e.getMessage(),e);
+        return RestResp.fail(e.getErrorCodeEnum());
+    }
+
+    /**
+     * 处理系统异常
+     * */
+    @ExceptionHandler(Exception.class)
+    public RestResp<Void> handlerException(Exception e){
+        log.error(e.getMessage(),e);
+        return RestResp.error();
+    }
+
+}
+
